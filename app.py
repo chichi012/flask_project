aebbf4fa54bdf8f12da03f8a3d21ee2f7a4580fa
Chidinma Afamefuna <chidinma@Chidinmas-MacBook-Pro.local>
2022-09-06 23:18:33 +0100

    working books api

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import json
+from book_model import *
+from settings import *
 from flask import Flask, jsonify, request, Response
 from settings import *
+
+import jwt
 '''
 1. Build first flask application API 
 2. First end-point '/books'
 3. Second end-point '/books/<ISBN>
 '''
-#
+
 # app = Flask(__name__)
 books = [
     {
@@ -26,19 +30,20 @@ books = [
 @app.route('/books')
 def get_books():
     # return a json instead of a list
-    return jsonify({'books': books})
+    return jsonify({'books': Book.get_all_books()})
 
 
 # GET /books/9782371000193
 @app.route('/books/<int:isbn>')
-def get_isbn(isbn):
-    return_value = {}
-    for book in books:
-        if book['isbn'] == isbn:
-            return_value = {
-                'name': book['name'],
-                'price': book['price']
-            }
+def get_book_by_isbn(isbn):
+    return_value = Book.get_book(isbn)
+    # return_value = {}
+    # for book in books:
+    #     if book['isbn'] == isbn:
+    #         return_value = {
+    #             'name': book['name'],
+    #             'price': book['price']
+    #         }
 
     return jsonify(return_value)
 
@@ -63,16 +68,17 @@ def valid_book_object(book_object):
 def add_books():
     request_data = request.get_json()
     if valid_book_object(request_data):
-        new_book = {
-            "name": request_data['name'],
-            "price": request_data['price'],
-            "isbn": request_data['isbn']
-        }
-        books.insert(0, new_book)
+        Book.add_book(request_data['name'], request_data['price'], request_data['isbn'])
+        # new_book = {
+        #     "name": request_data['name'],
+        #     "price": request_data['price'],
+        #     "isbn": request_data['isbn']
+        # }
+        # books.insert(0, new_book)
         response = Response("", 201, mimetype='application/json')
         # a Location header points to where the client can go and receivee the new resource that was created
         # expected link is /books/isbn_number
-        response.headers['Location'] = "/books/" + str(new_book['isbn'])
+        response.headers['Location'] = "/books/" + str(request_data['isbn'])
         return response  # response status code 200
     else:
         invalid_book_object_error_msg = {
@@ -98,21 +104,38 @@ b. valid book object has name and price field
 '''
 
 
+def valid_put_request_data(request_data):
+    if 'name' in request_data and 'price' in request_data:
+        return True
+    else:
+        return False
+
+
 @app.route('/books/<int:isbn>', methods=['PUT'])
 def replace_book(isbn):
     request_data = request.get_json()
-    new_book = {
-        "name": request_data['name'],
-        "price": request_data['price'],
-        "isbn": isbn
-    }
+    if (not valid_put_request_data(request_data)):
+        invalid_book_object_error_msg = {
+            "error": "Valid book object must be passed in the request",
+            "help_string": "Data passed in similar to this {'name': 'bookname', 'price': 7.99}"
+        }
+        response = Response(json.dumps(invalid_book_object_error_msg), status=400, mimetype='application/json')
+        return response
+
+    Book.replace_book(isbn, request_data['name'], request_data['price'])
+    # new_book = {
+    #     "name": request_data['name'],
+    #     "price": request_data['price'],
+    #     "isbn": isbn
+    # }
+
     # create a loop to search for the book in books
-    i = 0
-    for book in books:
-        current_isbn = book["isbn"]
-        if current_isbn == isbn:
-            books[i] = new_book
-        i += 1
+    # i = 0
+    # for book in books:
+    #     current_isbn = book["isbn"]
+    #     if current_isbn == isbn:
+    #         books[i] = new_book
+    #     i += 1
     response = Response("", status=204)
     return response
 
@@ -138,14 +161,16 @@ eg from
 def update_book(isbn):
     request_data = request.get_json()
     # updated_book will serve as a placeholder for all different properties that need updating
-    updated_book = {}
-    if ("name" in request_data):
-        updated_book["name"] = request_data["name"]
+    # updated_book = {}
     if ("price" in request_data):
-        updated_book["price"] = request_data["price"]
-    for book in books:
-        if book['isbn'] == isbn:
-            book.update(updated_book)
+        Book.update_book_price(isbn, request_data['price'])
+        # updated_book["price"] = request_data["price"]
+    if ("name" in request_data):
+        Book.update_book_name(isbn, request_data['name'])
+        # updated_book["name"] = request_data["name"]
+    # for book in books:
+    #     if book['isbn'] == isbn:
+    #         book.update(updated_book)
 
     response = Response("", status=204)
     response.headers['Location'] = "/books/" + str(isbn)
@@ -161,14 +186,17 @@ def update_book(isbn):
 
 @app.route('/books/<int:isbn>', methods=['DELETE'])
 def delete_book(isbn):
-    i = 0
-    for book in books:
-        if book['isbn'] == isbn:
-            # return jsonify(book)
-            books.pop(i)
-            response = Response("", status=204)
-            return response
-        i += 1
+    # i = 0
+    # for book in books:
+    #     if book['isbn'] == isbn:
+    #         # return jsonify(book)
+    #         books.pop(i)
+    #         response = Response("", status=204)
+    #         return response
+    #     i += 1
+    if Book.delete_book(isbn):
+        response = Response("", status=204)
+        return response
     invalid_book_object_error_msg = {
         "error": "Book with the ISBN Number that was provided was not found, therefore unable to delete"}
     response = Response(json.dumps(invalid_book_object_error_msg), status=404, mimetype="application/json")
@@ -178,6 +206,16 @@ def delete_book(isbn):
 ''' 
 STORING DATA IN A SQL DATABASE
 settings.py file will store the configuration for the database
+book_model contains the code for our database
+'''
+
+
+
+
+
+'''
+Adding 1thentication to our API
+
 '''
 
 app.run(port=5000)
